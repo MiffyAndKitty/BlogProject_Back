3d285063aa383c74ffe228d46de55ce44a96ac56
xEzIxX <by1094@gachon.ac.kr>
2024-05-23 15:55:48 +0000

    feat: 인증 서비스에 사용자 저장 및 토큰 삭제 기능 추가 1. deleteToken : 사용자 ID를 받아서 RefreshToken 테이블에서 해당 사용자의 토큰을 삭제 2. saveUser : UserDto를 받아서 User 테이블에 사용자 정보를 저장

---
+++ src/services/auth/auth.ts
@@ -0,0 +1,52 @@
+import { conn } from '../../loaders/mariadb';
+import { getHashed } from '../../utils/getHashed';
+import { UserDto } from '../../dtos';
+import { ensureError } from '../../errors/ensureError';
+
+export class AuthService {
+  static deleteToken = async (userid: string) => {
+    try {
+      const deleted = await conn.query(
+        'DELETE FROM RefreshToken WHERE token_userid=?;',
+        userid
+      );
+      if (deleted.affectedRows === 1) {
+        return { result: true, message: '로그아웃 성공' };
+      } else {
+        const message =
+          deleted.affectedRows === 0
+            ? '유효한 access 토큰, 존재하지 않는 refresh 토큰'
+            : '비정상적으로 많은 갯수의 refresh 토큰을 삭제';
+        return {
+          result: false,
+          message: message
+        };
+      }
+    } catch (err) {
+      const error = ensureError(err);
+      console.log(error.message);
+      return { result: false, message: error.message };
+    }
+  };
+
+  static saveUser = async (userDto: UserDto) => {
+    try {
+      const hashed = await getHashed(userDto.password);
+
+      const saved = await conn.query(
+        'INSERT INTO User (user_email, user_password, user_nickname) VALUES (?, ?, ?)',
+        [userDto.email, hashed, userDto.nickname]
+      );
+
+      if (saved.affectedRows === 1) {
+        return { result: true, message: '회원 정보 저장 성공' };
+      } else {
+        return { result: false, message: '회원 정보 저장 실패' };
+      }
+    } catch (err) {
+      const error = ensureError(err);
+      console.log(error.message);
+      return { result: false, message: error.message };
+    }
+  };
+}
