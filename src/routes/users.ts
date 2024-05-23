2ecfccde4dc563a2d17c2386ce58f7d6a6f755ec
xEzIxX <by1094@gachon.ac.kr>
2024-05-23 10:00:52 +0000

    feat : 사용자 정보 중복 여부를 확인하는 라우터, 서비스 함수 추가

---
+++ src/routes/users.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from 'express';
+import { ensureError } from '../errors/ensureError';
+import { BasicReturnType } from '../interfaces';
+import { validateDto } from '../middleware/validateDto';
+import { DbColumnDto } from '../dtos/dbColumn.dto';
+import { UsersService } from '../services/users';
+export const usersRouter = Router();
+
+usersRouter.post(
+  '/duplication',
+  await validateDto(DbColumnDto),
+  async (req: Request, res: Response) => {
+    try {
+      const result: BasicReturnType = await UsersService.isDuplicated(req.body);
+
+      if (result.result === true) {
+        return res.status(200).send(result);
+      } else {
+        return res.status(400).send(result);
+      }
+    } catch (err) {
+      const error = ensureError(err);
+      console.log(error.message);
+      return { result: false, message: error.message };
+    }
+  }
+);
