a30c52f677742ff957711c0d656f8ac8bc7704fc
xEzIxX <by1094@gachon.ac.kr>
2024-05-28 18:54:54 +0000

    feat : refresh 토큰을 redis에 저장하고, 가져오는 유틸 함수 추가

---
+++ src/utils/redis/refreshToken.ts
@@ -0,0 +1,25 @@
+import { redis } from '../../loaders/redis';
+import { ensureError } from '../../errors/ensureError';
+
+export async function setRefreshToken(userId: string, refreshToken: string) {
+  try {
+    return await redis.set(`refreshToken:${userId}`, refreshToken, {
+      EX: 7 * 24 * 60 * 60 // 7주일 뒤 만료
+    });
+  } catch (err) {
+    const error = ensureError(err);
+    console.log('refresh 토큰 저장 오류 : ', error.message);
+    return error.name;
+  }
+}
+
+export async function getRefreshToken(userId: string) {
+  try {
+    const key = `refreshToken:${userId}`;
+    return await redis.get(key);
+  } catch (err) {
+    const error = ensureError(err);
+    console.log('refresh 토큰 반환 오류 : ', error.message);
+    return error.name;
+  }
+}
