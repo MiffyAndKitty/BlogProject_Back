533fe2023a7af0568f98ed27a6419c68ed03a2ed
xEzIxX <by1094@gachon.ac.kr>
2024-05-30 08:05:00 +0000

    feat : 서버에 들어오는 요청 데이터 유효성 검증 미들웨어 추가

---
+++ src/middleware/express-validation.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { ContextRunner } from 'express-validator';
+
+export const validate = (validations: ContextRunner[]) => {
+  return async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    // sequential processing, stops running validations chain if one fails.
+    for (const validation of validations) {
+      const result = await validation.run(req);
+      if (!result.isEmpty()) {
+        const errors = result
+          .array()
+          .map((error) => error.msg)
+          .join(', ');
+        return res.status(400).send({
+          result: false,
+          message: `데이터 유효성 검증 실패: ${errors}`
+        });
+      }
+    }
+
+    next();
+  };
+};
