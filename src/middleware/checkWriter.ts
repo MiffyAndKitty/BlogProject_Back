b9aa6024697808eeec86ace8ac4e73539b3c4329
xEzIxX <by1094@gachon.ac.kr>
2024-06-24 14:13:56 +0000

    feat: 글 작성자 여부를 확인하는 checkWriter 미들웨어 추가 1. 사용자 ID 및 게시판 ID를 확인하여 작성자 여부를 확하는 미들웨어 추가

---
+++ src/middleware/checkWriter.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { db } from '../loaders/mariadb';
+
+export const checkWriter = () => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.id;
+      const boardId = req.params.boardId.split(':')[1];
+
+      req.isWriter = false;
+
+      if (!userId || !boardId) return next();
+
+      const existed = await db.query(
+        'SELECT * FROM Board WHERE user_id = ? AND board_id = ? LIMIT 1',
+        [userId, boardId]
+      );
+      if (existed.length > 0) req.isWriter = true;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
