9078299c0984a49fd4b2b46c0e6ac87b4520e753
xEzIxX <by1094@gachon.ac.kr>
2024-07-14 06:56:37 +0000

    refactor : 카테고리 서비스 수정 및 기능 개선 1. CategoryDto 인터페이스가 CategoryListDto 및 CategorySaveDto로 분리됨에 따른 수정 2. 게시글 카테고리 리스트 조회 시, 사용자의 닉네임이 한글일 경우를 고려하여 디코딩하고 소유자 정보 확인하도록 수정 3. Dto의 레벨 속성을 삭제하고, id의 맨 앞자리 숫자를 확인하여 레벨을 확인하도록 수정 4. 카테고리 레벨이 범위 내에 존재하지 않는다면 에러를 발생시키는 로직 추가 5. 카테고리 삭제 시 하위 카테고리도 삭제 처리 추가

--- src/services/category.ts
+++ src/services/category.ts
@@ -1,51 +1,103 @@
 import { db } from '../loaders/mariadb';
 import { ensureError } from '../errors/ensureError';
-import { CategoryDto } from '../interfaces/category';
+import {
+  CategoryDto,
+  CategoryListDto,
+  CategorySaveDto
+} from '../interfaces/category';
+import { v4 as uuidv4 } from 'uuid';
 
 export class categoryService {
-  static getList = async (categoryDto: CategoryDto) => {
+  static getList = async (categoryDto: CategoryListDto) => {
     try {
+      const decodedNickname = decodeURIComponent(categoryDto.nickname);
+
+      const [user] = await db.query(
+        // 카테고리 소유자
+        `SELECT user_id FROM User WHERE user_nickname= ? AND deleted_at IS NULL LIMIT 1;`,
+        [decodedNickname]
+      );
+
+      if (!user) {
+        return {
+          result: false,
+          data: null,
+          owner: false,
+          message: '존재하지 않는 카테고리 사용자'
+        };
+      }
+      const level = Number(categoryDto.topcategoryId?.charAt(0)) || -1;
+
       const list =
-        categoryDto.level === 0
+        level >= 0
           ? await db.query(
-              `SELECT category_id, category_name FROM Board_Category0 WHERE user_id = ? AND deleted_at IS NULL`,
-              [categoryDto.userId]
+              `SELECT category_id, category_name FROM Board_Category${level + 1} WHERE user_id = ? AND topcategory_id = ? AND deleted_at IS NULL`,
+              [user.user_id, categoryDto.topcategoryId]
             )
           : await db.query(
-              `SELECT category_id, category_name FROM Board_Category${categoryDto.level} WHERE topcategory_id = ? AND deleted_at IS NULL`,
-              [categoryDto.topcategoryId]
+              `SELECT category_id, category_name FROM Board_Category0 WHERE user_id = ? AND deleted_at IS NULL`,
+              [user.user_id]
             );
+      const owner = user.user_id === categoryDto.userId ? true : false;
 
       return list.length >= 0
         ? {
             result: true,
             data: list,
+            owner: owner,
             message: '게시글 카테고리 리스트 조회 성공'
           }
         : {
             result: false,
             data: null,
+            owner: false,
             message: '게시글 카테고리 리스트 조회 실패'
           };
     } catch (err) {
       const error = ensureError(err);
       console.log(error.message);
-      return { result: false, data: null, message: error.message };
+      return {
+        result: false,
+        data: null,
+        owner: false,
+        message: error.message
+      };
     }
   };
 
-  static create = async (categoryDto: CategoryDto) => {
+  static create = async (categoryDto: CategorySaveDto) => {
     try {
-      const created =
-        categoryDto.level === 0 || categoryDto.level === undefined
-          ? await db.query(
-              `INSERT INTO Board_Category0 (user_id, category_name) VALUES (?, ?) `,
-              [categoryDto.userId, categoryDto.categoryName]
-            )
-          : await db.query(
-              `INSERT INTO Board_Category${categoryDto.level} (topcategory_id, category_name) VALUES (?, ?) `,
-              [categoryDto.topcategoryId, categoryDto.categoryName]
-            );
+      let created, categoryId;
+
+      if (categoryDto.topcategoryId) {
+        // 상위 카테고리가 주어진 경우
+        const topLevel = Number(categoryDto.topcategoryId?.charAt(0));
+
+        console.log(topLevel);
+        //console.log(topLevel >= 0 || topLevel =< 2);
+
+        if (isNaN(topLevel) || topLevel < 0 || topLevel >= 2) {
+          throw new Error('상위 카테고리 레벨은 0또는 1이어야합니다.');
+        }
+
+        categoryId = Number(topLevel) + 1 + uuidv4().replace(/-/g, '');
+        created = await db.query(
+          `INSERT INTO Board_Category${Number(topLevel) + 1} (user_id, topcategory_id, category_id, category_name) VALUES (?,?,?,?) `,
+          [
+            categoryDto.userId,
+            categoryDto.topcategoryId,
+            categoryId,
+            categoryDto.categoryName
+          ]
+        );
+      } else {
+        categoryId = 0 + uuidv4().replace(/-/g, '');
+        created = await db.query(
+          `INSERT INTO Board_Category0 (user_id, category_id, category_name) VALUES (?,?,?) `,
+          [categoryDto.userId, categoryId, categoryDto.categoryName]
+        );
+      }
+
       if (created.affectedRows === 1) {
         return { result: true, message: '카테고리 저장 성공' };
       } else {
@@ -63,16 +115,20 @@ export class categoryService {
     }
   };
 
-  private static _restore = async (categoryDto: CategoryDto) => {
+  private static _restore = async (categoryDto: CategorySaveDto) => {
     try {
+      const level = Number(categoryDto.categoryId?.charAt(0));
+      if (level < 0 || level > 2) {
+        throw new Error('카테고리 레벨이 0 이상, 2 이하이어야 합니다.');
+      }
       const restored =
-        categoryDto.level === 0 || categoryDto.level === undefined
+        level === 0
           ? await db.query(
               `UPDATE Board_Category0 SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP WHERE user_id = ? AND category_name=?`,
               [categoryDto.userId, categoryDto.categoryName]
             )
           : await db.query(
-              `UPDATE Board_Category${categoryDto.level} SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP  WHERE topcategory_id = ? AND category_name=?`,
+              `UPDATE Board_Category${level} SET deleted_at = NULL, updated_at = CURRENT_TIMESTAMP  WHERE topcategory_id = ? AND category_name=?`,
               [categoryDto.topcategoryId, categoryDto.categoryName]
             );
       if (restored.affectedRows === 1) {
@@ -90,11 +146,15 @@ export class categoryService {
     }
   };
 
-  static modify = async (categoryDto: CategoryDto) => {
+  static modify = async (categoryDto: CategorySaveDto) => {
     try {
+      const level = Number(categoryDto.categoryId?.charAt(0));
+      if (level < 0 || level > 2) {
+        throw new Error('카테고리 레벨이 0 이상, 2 이하이어야 합니다.');
+      }
       const updated = await db.query(
-        `UPDATE Board_Category${categoryDto.level} SET category_name = ? WHERE category_id =?`,
-        [categoryDto.categoryName, categoryDto.categoryId]
+        `UPDATE Board_Category${level} SET category_name = ? WHERE category_id =? AND user_id=?`,
+        [categoryDto.categoryName, categoryDto.categoryId, categoryDto.userId]
       );
 
       return updated.affectedRows === 1
@@ -109,16 +169,32 @@ export class categoryService {
 
   static delete = async (categoryDto: CategoryDto) => {
     try {
-      const deleted = await db.query(
-        `UPDATE Board_Category${categoryDto.level} SET deleted_at = CURRENT_TIMESTAMP WHERE category_id =? AND deleted_at IS NULL`,
-        [categoryDto.categoryId]
+      let level = Number(categoryDto.categoryId?.charAt(0));
+      if (level < 0 || level > 2) {
+        throw new Error('카테고리 레벨이 0 이상, 2 이하이어야 합니다.');
+      }
+      let deleted = await db.query(
+        `UPDATE Board_Category${level} SET deleted_at = CURRENT_TIMESTAMP WHERE category_id = ? AND user_id= ? AND deleted_at IS NULL`,
+        [categoryDto.categoryId, categoryDto.userId]
       );
 
-      if (deleted.affectedRows === 1) {
-        return { result: true, message: '카테고리 삭제 성공' };
-      } else {
+      if (deleted.affectedRows === 0) {
         return { result: false, message: '카테고리 삭제 실패' };
       }
+
+      level++;
+
+      while (level > 0 && level <= 2) {
+        // 하위 카테고리도 모두 삭제
+        deleted = await db.query(
+          `UPDATE Board_Category${level} SET deleted_at = CURRENT_TIMESTAMP WHERE topcategory_id = ? AND deleted_at IS NULL`,
+          [categoryDto.categoryId]
+        );
+        if (deleted.affectedRows === 0) break;
+        level++;
+      }
+
+      return { result: true, message: '카테고리 삭제 성공' };
     } catch (err) {
       const error = ensureError(err);
       console.log(error.message);
