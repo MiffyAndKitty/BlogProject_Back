4ee325867810a15b15e755dca8d3616e855f0b1b
xEzIxX <by1094@gachon.ac.kr>
2024-07-14 06:54:05 +0000

    feat : app.ts 수정 1. 개발 모드에서의 요청 로깅을 위해 morgan 라이브러리 추가 2. 카테고리 라우터 추가

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import { dbConnector } from './loaders/mariadb';
 import passport from 'passport';
 import { passportLoader } from './passport';
 import { authRouter } from './routes/auth';
 import { usersRouter } from './routes/users';
 import { redisConnector } from './loaders/redis';
+import { categoryRouter } from './routes/category';
+
 const app = express();
 
 app.use(
@@ -17,6 +20,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(morgan('dev'));
 
 redisConnector;
 await dbConnector();
@@ -25,6 +29,8 @@ app.use(passport.initialize());
 await passportLoader();
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
+app.use('/category', categoryRouter);
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   return res
     .status(500)
