0607a102267c8fb5b055a71c38494c89886c1f82
xEzIxX <by1094@gachon.ac.kr>
2024-07-16 18:08:30 +0000

    feat : getBoard 메소드에 좋아요 및 조회수 기능 추가 1. _isLiked 메소드를 통해 사용자 존재하는 경우, 해당 글에 좋아요 여부를 확인 2. _addView 메소드를 통해 사용자가 게시글을 당일에 처음 조회하는 경우에 조회수를 증가하고,   캐시된 조회수를 조회된 게시글의 조회수에 더하여 최종 조회수를 반환하도록 수정

--- src/services/board/board.ts
+++ src/services/board/board.ts
@@ -1,6 +1,7 @@
 import { db } from '../../loaders/mariadb';
 import { ensureError } from '../../errors/ensureError';
 import { BoardIdInfoDto } from '../../interfaces/board/IdInfo';
+import { redis } from '../../loaders/redis';
 
 export class BoardService {
   static getBoard = async (boardIdInfoDto: BoardIdInfoDto) => {
@@ -9,26 +10,25 @@ export class BoardService {
         'SELECT * from Board WHERE board_id = ?  AND deleted_at IS NULL LIMIT 1',
         [boardIdInfoDto.boardId]
       );
+      if (!data) throw new Error('존재하지 않는 게시글입니다.');
 
-      if (data) {
-        data.isWriter = Boolean(data.user_id === boardIdInfoDto.userId);
-        const tagData = await db.query(
-          'SELECT tag_name FROM Board_Tag WHERE board_id = ? AND deleted_at IS NULL',
-          [boardIdInfoDto.boardId]
-        );
-        data.tags = tagData.map((row: any) => row.tag_name);
-        return {
-          result: true,
-          data: data,
-          message: '게시글 데이터 조회 성공'
-        };
-      } else {
-        return {
-          result: false,
-          data: null,
-          message: '게시글 데이터 조회 실패'
-        };
-      }
+      const tagData = await db.query(
+        'SELECT tag_name FROM Board_Tag WHERE board_id = ? AND deleted_at IS NULL',
+        [boardIdInfoDto.boardId]
+      );
+      data.tags = tagData.map((row: any) => row.tag_name);
+
+      data.isWriter = Boolean(data.user_id === boardIdInfoDto.userId);
+      data.isLike = boardIdInfoDto.userId
+        ? await BoardService._isLiked(boardIdInfoDto)
+        : false;
+      data.board_view += await BoardService._addView(boardIdInfoDto);
+
+      return {
+        result: true,
+        data: data,
+        message: '게시글 데이터 조회 성공'
+      };
     } catch (err) {
       const error = ensureError(err);
       console.log(error.message);
@@ -56,6 +56,46 @@ export class BoardService {
       const error = ensureError(err);
       console.log(error.message);
       return { result: false, message: error.message };
+    }
+  };
+
+  // 사용자의 좋아요 여부 확인
+  private static _isLiked = async (boardIdInfoDto: BoardIdInfoDto) => {
+    let isLike = false;
+
+    // redis에서 사용자가 좋아요를 눌렀는지 확인
+    const isLikedInRedis = await redis.SISMEMBER(
+      `boardLike:${boardIdInfoDto.boardId}`,
+      boardIdInfoDto.userId
+    );
+
+    // redis에 사용자 정보가 없는 경우, DB에서 사용자가 좋아요 정보를 확인
+    if (!isLikedInRedis) {
+      const isLikedInDB = await db.query(
+        'SELECT 1 FROM Board_Like WHERE board_id = ? AND user_id = ? AND deleted_at IS NULL',
+        [boardIdInfoDto.boardId, boardIdInfoDto.userId]
+      );
+      isLike = isLikedInDB.length > 0;
+    }
+    if (isLike == false) isLike = isLikedInRedis;
+    return isLike;
+  };
+
+  // resid의 sets 자료형을 이용하여 boardId(key)에 userId(value)들을 저장
+  // 캐시된 데이터들은 매일 자정이 지날 때마다 board의 view칼럼에 value들의 숫자를 더하여 저장되도록 업데이트 될 것임
+  private static _addView = async (boardIdInfoDto: BoardIdInfoDto) => {
+    const { boardId, userId } = boardIdInfoDto;
+    const redisKey = `boardView:${boardId}`;
+
+    // Redis에 조회수 +1 캐시
+    if (userId) {
+      const isAdded = await redis.sAdd(redisKey, userId);
+      isAdded === 0
+        ? console.log('이미 조회한 유저')
+        : console.log('처음 조회한 유저');
+
+      const viewCount = await redis.SCARD(redisKey);
+      return viewCount;
     }
   };
 }
