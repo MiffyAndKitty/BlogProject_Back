2316e2b07c4e839a3754c71d67c472be4261cbfd
xEzIxX <by1094@gachon.ac.kr>
2024-07-29 02:32:03 +0000

    feat : 게시글 리스트 정렬 서비스 함수에 사용되는 HierarchicalCategoryDto 추가

--- src/interfaces/category.ts
+++ src/interfaces/category.ts
@@ -10,3 +10,11 @@ export interface CategoryListDto {
   userId?: string;
   topcategoryId?: string;
 }
+
+export interface HierarchicalCategoryDto {
+  // 게시글 리스트 정렬 서비스 함수
+  category_id: string;
+  category_name: string;
+  topcategory_id?: string;
+  subcategories?: HierarchicalCategoryDto[];
+}
