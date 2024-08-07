ed8c60cb7f7ca9d428caeb2a1984d4ad8a73e26a
xEzIxX <by1094@gachon.ac.kr>
2024-08-07 06:24:47 +0000

    refactor : ioredis라이브러리로 전환함에 따른 config파일, loader파일 수정

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,11 +1,7 @@
 import './env';
-import { createClient } from 'redis';
 
-export const client = createClient({
-  socket: {
-    port: parseInt(process.env.REDIS_PORT!),
-    host: process.env.REDIS_HOST
-  },
-  password: process.env.REDIS_PASSWORD,
-  legacyMode: true // 이전 버전과의 호환성 유지
-});
+export const redisConfig = {
+  port: parseInt(process.env.REDIS_PORT!),
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASSWORD
+};
