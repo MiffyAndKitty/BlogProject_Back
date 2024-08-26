f43379dc5f868bfcd9c8e477c3e9db62213620f1
xEzIxX <by1094@gachon.ac.kr>
2024-08-26 03:58:47 +0000

    refactor : 댓글 좋아요/싫어요 처리 로직 개선 1. Redis 캐시를 사용하여 댓글 좋아요/싫어요 추가 및 삭제 로직 최적화 2. 댓글 좋아요/ 싫어요 추가   (1) DB에서 이미 좋아요/싫어요 상태를 확인 후, 좋아요/싫어요를 캐시 3. 댓글 좋아요/싫어요 삭제   (1) 캐시된 데이터 확인 후 DB에서 soft delete하도록 수정

--- src/services/comment/comment.ts
+++ src/services/comment/comment.ts
@@ -7,6 +7,7 @@ import {
   CommentIdDto,
   CommentLikeDto
 } from '../../interfaces/comment';
+import { redis } from '../../loaders/redis';
 
 export class commentService {
   // 댓글 생성
@@ -86,6 +87,48 @@ export class commentService {
   // 댓글 좋아요 또는 싫어요 추가
   static like = async (commentLikeDto: CommentLikeDto) => {
     try {
+      // DB에서 사용자가 이미 좋아요를 눌렀는지 확인
+      const [likedInDB] = await db.query(
+        'SELECT comment_like FROM Comment_Like WHERE comment_id = ? AND user_id = ? AND deleted_at IS NULL',
+        [commentLikeDto.commentId, commentLikeDto.userId]
+      );
+
+      if (!likedInDB) {
+        // Redis에 좋아요 캐시 추가 ( DB에 없을 때만 추가 )
+        const likedInRedis = await redis.hset(
+          `comment_like:${commentLikeDto.commentId}`,
+          commentLikeDto.userId,
+          Number(commentLikeDto.isLike)
+        );
+
+        // likedInRedis가 1이면 추가 성공, 0이지만 필드가 존재하면 수정 성공
+        if (
+          likedInRedis === 1 ||
+          (likedInRedis === 0 &&
+            (await redis.hexists(
+              `comment_like:${commentLikeDto.commentId}`,
+              commentLikeDto.userId
+            )))
+        ) {
+          return {
+            result: true,
+            message:
+              likedInRedis === 1 ? '좋아요 추가 성공' : '좋아요 수정 성공'
+          };
+        }
+
+        return { result: false, message: '좋아요 추가 실패 ( 캐시 실패 )' };
+      }
+
+      if (likedInDB.comment_like === commentLikeDto.isLike) {
+        return {
+          result: true,
+          message: commentLikeDto.isLike
+            ? '이미 좋아요한 댓글입니다'
+            : '이미 싫어요한 댓글입니다'
+        };
+      }
+      // likedInDB.comment_like !== commentLikeDto.isLike
       const query = `INSERT INTO Comment_Like (comment_id, user_id, comment_like) VALUES (?,?,?) 
                      ON DUPLICATE KEY UPDATE comment_like = ?, deleted_at = NULL`;
       const params = [
@@ -117,15 +160,27 @@ export class commentService {
     }
   };
 
-  // 댓글 좋아요 또는 싫어요 삭제
   static unlike = async (commentIdDto: CommentIdDto) => {
     try {
-      const query = `UPDATE Comment_Like SET deleted_at = CURRENT_TIMESTAMP WHERE comment_id = ? AND user_id = ? AND deleted_at IS NULL`;
+      // Redis에서 캐시 확인 후 삭제 시도
+      const isCashed = await redis.hdel(
+        `comment_like:${commentIdDto.commentId}`,
+        commentIdDto.userId
+      );
+
+      if (isCashed === 1)
+        return { result: true, message: '캐시된 댓글 좋아요/싫어요 삭제 성공' };
+
+      // 캐시에서 삭제되지 않았을 경우 DB에서 삭제 시도
+      const query = `
+      UPDATE Comment_Like 
+      SET deleted_at = CURRENT_TIMESTAMP 
+      WHERE comment_id = ? AND user_id = ? AND deleted_at IS NULL`;
       const params = [commentIdDto.commentId, commentIdDto.userId];
 
       const { affectedRows: deletedLikeCount } = await db.query(query, params);
 
-      return deletedLikeCount === 1
+      return deletedLikeCount > 0
         ? { result: true, message: '댓글 좋아요/싫어요 삭제 성공' }
         : { result: false, message: '댓글 좋아요/싫어요 삭제 실패' };
     } catch (err) {
