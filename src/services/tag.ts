3349668c4db04b57887b558d209fcfbb0f80a944
xEzIxX <by1094@gachon.ac.kr>
2024-09-01 19:24:21 +0000

    refactor: 인기 태그 조회 및 캐싱 로직 개선 - 개선 배경 : TagCacheJobService에서 만약 1시간 내에 생성된 태그가 없을 경우 score가 0인 대체 태그를 반환하도록 수정 - 수정 이전 상황 : tagService에서 캐시된 인기 태그를 score 없이 태그만 순서대로 반환함 - 수정 사항 : tagService에서 캐시된 인기 태그를 이름과 함께 score를 반환하도록 수정

--- src/services/tag.ts
+++ src/services/tag.ts
@@ -6,19 +6,37 @@ export class tagService {
   static getPopularList = async (tagDto: TagDto) => {
     try {
       const key = 'tag_popular';
-      const tagNames = await redis.zrevrange(key, 0, tagDto.limit);
+      // 태그 이름과 함께 점수를 반환
+      const cashedTags = await redis.zrevrange(
+        key,
+        0,
+        tagDto.limit - 1,
+        'WITHSCORES'
+      );
 
-      return tagNames
-        ? {
-            result: true,
-            data: tagNames,
-            message: '인기 태그 조회 성공'
-          }
-        : {
-            result: false,
-            data: [],
-            message: '인기 태그 조회 실패'
-          };
+      if (!cashedTags || cashedTags.length === 0) {
+        return {
+          result: false,
+          data: [],
+          message: '인기 태그 조회 실패'
+        };
+      }
+
+      // 태그와 점수를 객체 형태로 변환
+      const tagWithScores = [];
+      for (let i = 0; i < cashedTags.length; i += 2) {
+        // `cashedTags` 배열을 순회
+        tagWithScores.push({
+          tagName: cashedTags[i],
+          score: Number(cashedTags[i + 1])
+        });
+      }
+
+      return {
+        result: true,
+        data: tagWithScores,
+        message: '인기 태그 조회 성공'
+      };
     } catch (err) {
       const error = ensureError(err);
       console.log(error.message);
