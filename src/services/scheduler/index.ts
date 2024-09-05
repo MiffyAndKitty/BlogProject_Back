de73ec6256bfbd8377d2ccc2c2f9eb200892c267
xEzIxX <by1094@gachon.ac.kr>
2024-09-05 04:05:19 +0000

    refactor: 다양한 주기적인 작업들을 관리하고 실행하는 스케줄러 클래스 수정
    - `_logJobResult` 함수를 추가하여 작업 성공/실패 로그 처리를 공통화, 코드 중복 제거
    - `cacheTopFollowersWeekly` 메서드 추가: 최다 팔로워 보유 블로거를 주간 단위로 캐싱하는 작업을 추가

--- src/services/scheduler/index.ts
+++ src/services/scheduler/index.ts
@@ -1,45 +1,68 @@
 import { TagCacheJobService } from './tagCacheJob';
 import { BoardUpdateJobService } from './boardUpdateJob';
 import { CommentUpdateJobService } from './commentUpdateJob';
+import { TopFollowersCacheJobService } from './topFollowerCacheJob';
 
 export class JobScheduler {
-  public static dailyUpdate = async () => {
-    try {
-      console.log(
-        '[24시간 마다 실행] 조회수/좋아요수 DB에 업데이트 : ',
-        new Date()
-      );
+  private static _logJobResult(jobName: string, isSuccess: boolean) {
+    isSuccess
+      ? console.log(`${jobName} 작업 성공 [${new Date()}]`)
+      : console.error(`${jobName} 작업 실패 [${new Date()}]`);
+  }
 
-      // 작업을 병렬로 실행, 실패하더라도 나머지 작업이 성공하도록 기다림
+  public static updateBoardMetrics = async () => {
+    try {
+      const jobNames = ['게시글 조회수 업데이트', '게시글 좋아요 업데이트'];
       const results = await Promise.allSettled([
         BoardUpdateJobService.updateBoard('board_view'),
-        BoardUpdateJobService.updateBoard('board_like'),
-        CommentUpdateJobService.updateComment('comment_like')
+        BoardUpdateJobService.updateBoard('board_like')
       ]);
 
-      // 각 작업의 결과를 처리
       results.forEach((result, index) => {
-        const jobNames = [
-          '게시글 조회수 업데이트',
-          '게시글 좋아요 업데이트',
-          '댓글 좋아요 업데이트'
-        ];
-
-        console.log(
-          `${jobNames[index]} ${result.status === 'fulfilled' && result.value ? '성공' : '실패'}`
+        this._logJobResult(
+          jobNames[index],
+          result.status === 'fulfilled' && result.value
         );
       });
     } catch (err) {
-      console.error('DB 업데이트 작업을 병렬로 실행하는 중 오류가 발생', err);
+      console.error('[ERROR] 게시글 메트릭 업데이트 중 에러 발생:', err);
     }
   };
 
-  public static hourlyUpdate = async (limit: number = 10) => {
+  public static updateCommentMetrics = async () => {
     try {
-      await TagCacheJobService.cacheTags('tag_popular', limit);
-      console.log('인기 태그 캐싱 성공');
+      const jobName = '댓글 좋아요 업데이트';
+      const result =
+        await CommentUpdateJobService.updateComment('comment_like');
+      this._logJobResult(jobName, result);
     } catch (err) {
-      console.error('인기 태그 캐싱 중 오류 발생:', err);
+      console.error('[ERROR] 댓글 메트릭 업데이트 중 에러 발생:', err);
+    }
+  };
+
+  public static cachePopularTags = async (limit: number = 10) => {
+    const jobName = '인기 태그 캐싱';
+    try {
+      const isSuccess = await TagCacheJobService.cacheTags(
+        'tag_popular',
+        limit
+      );
+      this._logJobResult(jobName, isSuccess);
+    } catch (err) {
+      console.error(`[ERROR] ${jobName} 작업 중 에러 발생:`, err);
+    }
+  };
+
+  public static cacheTopFollowersWeekly = async (limit: number = 10) => {
+    const jobName = '최다 팔로워 보유 블로거 캐싱';
+    try {
+      const isSuccess = await TopFollowersCacheJobService.cacheTopFollowers(
+        'top-followers',
+        limit
+      );
+      this._logJobResult(jobName, isSuccess);
+    } catch (err) {
+      console.error(`[ERROR] ${jobName} 작업 중 에러 발생:`, err);
     }
   };
 }
