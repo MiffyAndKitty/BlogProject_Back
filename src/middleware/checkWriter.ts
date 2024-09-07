255fe3d9534318260443640c232a987019aa2c86
xEzIxX <by1094@gachon.ac.kr>
2024-09-07 20:21:05 +0000

    refactor: `checkWriter` 미들웨어에 MongoDB 임시 저장 게시글 작성자 확인 로직 추가
    - MySQL과 MongoDB 작성자 확인 로직을 하나의 미들웨어로 통합하여 처리
    - MongoDB에서 주어진 조건을 만족하는 문서의 개수를 반환하는 `countDocuments` 메서드를 사용해 임시 저장 게시글의 작성자를 확인

--- src/middleware/checkWriter.ts
+++ src/middleware/checkWriter.ts
@@ -1,21 +1,39 @@
 import { Request, Response, NextFunction } from 'express';
 import { db } from '../loaders/mariadb';
+import { mongodb } from '../loaders/mongodb';
+import { ObjectId } from 'mongodb';
 
-export const checkWriter = () => {
+export const checkWriter = (isDraft: boolean = false) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId = req.id;
-      const boardId = req.params.boardId?.split(':')[1] ?? req.body.boardId;
+      const boardId =
+        req.params.boardId?.split(':')[1] ??
+        req.body.boardId ??
+        req.params.draftId?.split(':')[1] ??
+        req.body.draftId;
 
-      req.isWriter = false;
+      if (!userId || !boardId) {
+        req.isWriter = false;
+        return next();
+      }
 
-      if (!userId || !boardId) return next();
-
-      const existed = await db.query(
-        'SELECT * FROM Board WHERE user_id = ? AND board_id = ? AND deleted_at IS NULL LIMIT 1',
-        [userId, boardId]
-      );
-      if (existed.length > 0) req.isWriter = true;
+      let isWriter = false;
+      if (isDraft) {
+        const draftCollection = mongodb.db('board_db').collection('drafts');
+        const count = await draftCollection.countDocuments({
+          _id: new ObjectId(boardId),
+          userId: userId
+        });
+        isWriter = count > 0;
+      } else {
+        const [board] = await db.query(
+          'SELECT 1 FROM Board WHERE user_id = ? AND board_id = ? AND deleted_at IS NULL LIMIT 1',
+          [userId, boardId]
+        );
+        isWriter = board ? true : false;
+      }
+      req.isWriter = isWriter;
       next();
     } catch (err) {
       next(err);
