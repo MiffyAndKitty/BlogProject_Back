2348fef1391c1195767fdd66037fa48c57530933
xEzIxX <by1094@gachon.ac.kr>
2024-09-07 20:41:00 +0000

    feat: 게시글 임시 저장, 수정 및 조회 기능 추가
    - `saveDraft` 메서드를 통해 게시글을 MongoDB에 임시 저장하는 기능 추가
    - `modifyDraft` 메서드를 통해 기존 임시 저장된 게시글 수정 기능 추가
    - `getDraft` 메서드를 통해 특정 게시글의 임시 저장된 상태를 조회하는 기능 추가

--- src/services/board/saveBoard.ts
+++ src/services/board/saveBoard.ts
@@ -2,9 +2,21 @@ import '../../config/env';
 import { db } from '../../loaders/mariadb';
 import { boardDto, modifiedBoardDto } from '../../interfaces/board/board';
 import { v4 as uuidv4 } from 'uuid';
-import { SingleNotificationResponse } from '../../interfaces/response';
 import { NotificationName } from '../../constants/notificationName';
 import { InternalServerError } from '../../errors/internalServerError';
+import { mongodb } from '../../loaders/mongodb';
+import { ObjectId } from 'mongodb';
+import { ensureError } from '../../errors/ensureError';
+import {
+  BasicResponse,
+  SingleNotificationResponse
+} from '../../interfaces/response';
+import {
+  DraftDto,
+  DraftIdDto,
+  UpdateDraftDto
+} from '../../interfaces/board/draft';
+import { NotFoundError } from '../../errors/notFoundError';
 
 export class saveBoardService {
   static modifyBoard = async (
@@ -121,6 +133,109 @@ export class saveBoardService {
         }
       },
       message: '게시글 저장 성공'
+    };
+  };
+
+  static saveDraft = async (draftDto: DraftDto): Promise<BasicResponse> => {
+    const draftCollection = mongodb.db('board_db').collection('drafts');
+    const draftId = new ObjectId();
+
+    let content = draftDto.content;
+    if (draftDto.fileUrls && draftDto.fileUrls.length > 0 && content) {
+      const urlReplaced: false | string = await saveBoardService._savedImage(
+        content,
+        draftDto.fileUrls
+      );
+      if (urlReplaced) content = urlReplaced;
+    }
+
+    const result = await draftCollection.insertOne({
+      _id: draftId,
+      userId: draftDto.userId,
+      title: draftDto.title,
+      content: content,
+      public: draftDto.public,
+      categoryId: draftDto.categoryId,
+      tagNames: draftDto.tagNames || [],
+      updatedAt: new Date()
+    });
+
+    if (!result.acknowledged) {
+      throw new InternalServerError('임시 저장 중 오류가 발생하였습니다.');
+    }
+    return { result: true, message: '임시 저장에 성공하였습니다.' };
+  };
+
+  static modifyDraft = async (
+    updateDraftDto: UpdateDraftDto
+  ): Promise<BasicResponse> => {
+    const draftCollection = mongodb.db('board_db').collection('drafts');
+    const draftId = new ObjectId(updateDraftDto.draftId);
+
+    const existingDraftCount = await draftCollection.countDocuments({
+      _id: draftId
+    });
+    if (existingDraftCount === 0)
+      throw new NotFoundError(
+        '해당 ID의 임시 저장된 게시글을 찾을 수 없습니다.'
+      );
+
+    let content = updateDraftDto.content;
+    if (
+      updateDraftDto.fileUrls &&
+      updateDraftDto.fileUrls.length > 0 &&
+      content
+    ) {
+      const urlReplaced: false | string = await saveBoardService._savedImage(
+        content,
+        updateDraftDto.fileUrls
+      );
+      if (urlReplaced) content = urlReplaced;
+    }
+
+    const result = await draftCollection.updateOne(
+      { _id: draftId },
+      {
+        $set: {
+          userId: updateDraftDto.userId,
+          title: updateDraftDto.title,
+          content: content,
+          public: updateDraftDto.public,
+          categoryId: updateDraftDto.categoryId,
+          tagNames: updateDraftDto.tagNames || [],
+          updatedAt: new Date()
+        }
+      }
+    );
+
+    if (result.modifiedCount === 0)
+      throw new InternalServerError(
+        '임시 저장된 게시글이 수정되지 않았습니다.'
+      );
+
+    return {
+      result: true,
+      message: '임시 저장된 게시글 수정에 성공하였습니다.'
+    };
+  };
+
+  static getDraft = async (draftIdDto: DraftIdDto) => {
+    const objectId = new ObjectId(draftIdDto.draftId);
+    const draftCollection = mongodb.db('board_db').collection('drafts');
+
+    const draft = await draftCollection.findOne({
+      _id: objectId
+    });
+
+    if (!draft)
+      throw new NotFoundError(
+        '해당 id의 임시 저장된 게시글을 찾지 못하였습니다.'
+      );
+
+    return {
+      result: true,
+      data: draft,
+      message: '임시 저장된 게시글 반환에 성공하였습니다.'
     };
   };
 
