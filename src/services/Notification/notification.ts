e27f45a9609349ea6b005a90e6477688876fddb2
xEzIxX <by1094@gachon.ac.kr>
2024-09-09 19:47:33 +0000

    refactor: 쿼리 빌드 로직 분리
    - getAll 함수의 쿼리 빌드 로직을 _buildQuery 및 _getCursorOrder 함수로 분리
      - _buildQuery: 페이지네이션, 정렬 및 필터링 조건에 따라 쿼리와 파라미터를 동적으로 생성하는 함수
      - _getCursorOrder: 주어진 커서를 기준으로 알림 목록의 순서를 가져오는 함수

--- src/services/Notification/notification.ts
+++ src/services/Notification/notification.ts
@@ -16,71 +16,9 @@ export class NotificationService {
       const pageSize = listDto.pageSize || 10;
       const totalPageCount = Math.ceil(totalCount / pageSize);
 
-      let query = `
-      SELECT 
-        Notifications.notification_id, 
-        Notifications.notification_recipient, 
-        Notifications.notification_type, 
-        Notifications.notification_read, 
-        Notifications.notification_trigger, 
-        Notifications.created_at, 
-        Notifications.updated_at, 
-        Notifications.notification_order,     
-        User.user_nickname AS trigger_nickname, 
-        User.user_email AS trigger_email, 
-        User.user_image AS trigger_image,
-        CASE
-          WHEN Notifications.notification_type IN ('${NotificationName.REPLY_TO_COMMENT}', '${NotificationName.COMMENT_ON_BOARD}') THEN Notifications.notification_location
-          ELSE NULL
-        END AS notification_comment,
-        Comment.parent_comment_id AS parent_comment_id,
-        CASE
-          WHEN Notifications.notification_type IN ('${NotificationName.FOLLOWING_NEW_BOARD}', '${NotificationName.BOARD_NEW_LIKE}') THEN Notifications.notification_location
-          WHEN Notifications.notification_type IN ('${NotificationName.REPLY_TO_COMMENT}', '${NotificationName.COMMENT_ON_BOARD}') THEN Comment.board_id
-          ELSE NULL
-        END AS notification_board,
-        Board.board_title AS board_title,
-        BoardUser.user_nickname AS board_writer,
-        Comment.comment_content AS comment_content 
-      FROM Notifications
-      JOIN User ON Notifications.notification_trigger = User.user_id
-      LEFT JOIN Comment ON Notifications.notification_location = Comment.comment_id
-      LEFT JOIN Board ON 
-         -- CASE 문에서 반환한 notification_board 값을 통해 Board와 조인
-          (CASE 
-              WHEN Notifications.notification_type IN ('${NotificationName.FOLLOWING_NEW_BOARD}', '${NotificationName.BOARD_NEW_LIKE}') THEN Notifications.notification_location
-              WHEN Notifications.notification_type IN ('${NotificationName.REPLY_TO_COMMENT}', '${NotificationName.COMMENT_ON_BOARD}') THEN Comment.board_id
-              ELSE NULL
-          END) = Board.board_id
-      LEFT JOIN User AS BoardUser ON Board.user_id = BoardUser.user_id 
-      WHERE Notifications.notification_recipient = ? 
-        ${sortQuery}
-        AND Notifications.deleted_at IS NULL
-    `;
-
-      const params: (string | number)[] = [listDto.userId];
-
-      let order = 'DESC';
-      // 커서가 있을 경우 커서 이후의 데이터를 가져오기 위해 조건 추가
-      if (listDto.cursor) {
-        const [{ notification_order: cursorOrder }] = await db.query(
-          `SELECT notification_order 
-         FROM Notifications 
-         WHERE notification_id = ? ${sortQuery};`,
-          [listDto.cursor]
-        );
-
-        if (!cursorOrder) throw new Error('유효하지 않은 커서입니다.');
-
-        query += ` AND notification_order ${listDto.isBefore ? '>' : '<'} ?`;
-        params.push(cursorOrder);
-
-        if (listDto.isBefore) order = 'ASC';
-      }
-
-      query += ` ORDER BY notification_order ${order} LIMIT ?`;
-      params.push(pageSize);
+      const { query, params } = await this._buildQuery(listDto, sortQuery);
       const result = await db.query(query, params);
+
       if (listDto.cursor && listDto.isBefore) result.reverse();
       return {
         result: true,
@@ -140,5 +78,84 @@ export class NotificationService {
       [userId]
     );
     return Number(countResult.totalCount.toString());
+  }
+  // 쿼리 및 파라미터 빌드
+  private static async _buildQuery(
+    listDto: NotificationListDto,
+    sortQuery: string
+  ) {
+    const pageSize = listDto.pageSize || 10;
+    const params: (string | number)[] = [listDto.userId];
+    let order = 'DESC';
+
+    let query = `
+      SELECT 
+        Notifications.notification_id, 
+        Notifications.notification_recipient, 
+        Notifications.notification_type, 
+        Notifications.notification_read, 
+        Notifications.notification_trigger, 
+        Notifications.created_at, 
+        Notifications.updated_at, 
+        Notifications.notification_order,     
+        User.user_nickname AS trigger_nickname, 
+        User.user_email AS trigger_email, 
+        User.user_image AS trigger_image,
+        CASE
+          WHEN Notifications.notification_type IN ('${NotificationName.REPLY_TO_COMMENT}', '${NotificationName.COMMENT_ON_BOARD}') THEN Notifications.notification_location
+          ELSE NULL
+        END AS notification_comment,
+        Comment.parent_comment_id AS parent_comment_id,
+        CASE
+          WHEN Notifications.notification_type IN ('${NotificationName.FOLLOWING_NEW_BOARD}', '${NotificationName.BOARD_NEW_LIKE}') THEN Notifications.notification_location
+          WHEN Notifications.notification_type IN ('${NotificationName.REPLY_TO_COMMENT}', '${NotificationName.COMMENT_ON_BOARD}') THEN Comment.board_id
+          ELSE NULL
+        END AS notification_board,
+        Board.board_title AS board_title,
+        BoardUser.user_nickname AS board_writer,
+        Comment.comment_content AS comment_content 
+      FROM Notifications
+      JOIN User ON Notifications.notification_trigger = User.user_id
+      LEFT JOIN Comment ON Notifications.notification_location = Comment.comment_id
+      LEFT JOIN Board ON 
+          (CASE 
+              WHEN Notifications.notification_type IN ('${NotificationName.FOLLOWING_NEW_BOARD}', '${NotificationName.BOARD_NEW_LIKE}') THEN Notifications.notification_location
+              WHEN Notifications.notification_type IN ('${NotificationName.REPLY_TO_COMMENT}', '${NotificationName.COMMENT_ON_BOARD}') THEN Comment.board_id
+              ELSE NULL
+          END) = Board.board_id
+      LEFT JOIN User AS BoardUser ON Board.user_id = BoardUser.user_id 
+      WHERE Notifications.notification_recipient = ? 
+        ${sortQuery}
+        AND Notifications.deleted_at IS NULL
+    `;
+
+    if (listDto.cursor) {
+      const cursorOrder = await this._getCursorOrder(listDto.cursor, sortQuery);
+      query += ` AND notification_order ${listDto.isBefore ? '>' : '<'} ?`;
+      params.push(cursorOrder);
+
+      if (listDto.isBefore) order = 'ASC';
+    }
+
+    query += ` ORDER BY notification_order ${order} LIMIT ?`;
+    params.push(pageSize);
+
+    return { query, params };
+  }
+
+  // 커서 정보 조회
+  private static async _getCursorOrder(
+    cursor: string,
+    sortQuery: string
+  ): Promise<number> {
+    const [{ notification_order: cursorOrder }] = await db.query(
+      `SELECT notification_order 
+       FROM Notifications 
+       WHERE notification_id = ? ${sortQuery};`,
+      [cursor]
+    );
+
+    if (!cursorOrder) throw new Error('유효하지 않은 커서입니다.');
+    return cursorOrder;
   }
 }
