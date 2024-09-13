6f65f9c1c17942109b767be476d15306e0b7ad67
xEzIxX <by1094@gachon.ac.kr>
2024-09-13 04:30:43 +0000

    refactor: verifyTokenError 함수 리팩토링 및 에러 처리 개선
    - 기존의 if-else 구문을 switch 문으로 변환하여 가독성 개선
    - ensureError 함수를 사용해 에러를 일관된 형식으로 처리
    - 각 에러에 대한 명확한 메시지를 UnauthorizedError 및 BadRequestError로 반환

--- src/errors/verifyTokenError.ts
+++ src/errors/verifyTokenError.ts
@@ -1,20 +1,17 @@
-export function verifyTokenError(err: unknown): Error {
-  if (!(err instanceof Error)) {
-    let stringified = '에러값을 문자열화 실패';
-    try {
-      stringified = JSON.stringify(err);
-    } catch {}
+import { UnauthorizedError } from './unauthorizedError';
+import { BadRequestError } from './badRequestError';
+import { ensureError } from './ensureError';
 
-    const error = new Error(`error : ${stringified}`);
-    return error;
-  } else {
-    if (err.name === 'TokenExpiredError') {
-      err.message = '만료된 토큰';
-    } else if (err.name === 'JsonWebTokenError') {
-      err.message = '유효하지 않은 토큰';
-    } else if (err.name === 'TypeError') {
-      err.message = '잘못된 타입의 토큰';
-    }
-    return err;
+export function verifyTokenError(err: unknown): Error {
+  const error = ensureError(err);
+  switch (error.name) {
+    case 'TokenExpiredError':
+      return new UnauthorizedError('만료된 토큰입니다.');
+    case 'JsonWebTokenError':
+      return new BadRequestError('유효하지 않은 토큰입니다.');
+    case 'TypeError':
+      return new BadRequestError('잘못된 타입의 토큰입니다.');
+    default:
+      return error;
   }
 }
