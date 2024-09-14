a3dd92129f4ee01100abb02ffc4dc1ed9a3ed5a3
xEzIxX <by1094@gachon.ac.kr>
2024-09-14 17:46:38 +0000

    refactor: auth 서비스 함수에서 try-catch 구문 제거
    - 예외 처리를 상위 레벨에서 일관되게 처리하도록 개선
    - 개별 서비스 함수에서 불필요한 try-catch 구문 삭제

--- src/services/auth/passport-local-login.ts
+++ src/services/auth/passport-local-login.ts
@@ -6,38 +6,28 @@ import { LoginServiceDto } from '../../interfaces/auth';
 export const localAuthService = async (
   user: LoginServiceDto
 ): Promise<MultipleUserDataResponse> => {
-  try {
-    const accessToken = newToken.access(user.userId);
-    const refreshToken = newToken.refresh();
+  const accessToken = newToken.access(user.userId);
+  const refreshToken = newToken.refresh();
 
-    if (typeof accessToken !== 'string' || typeof refreshToken !== 'string') {
-      return {
-        result: false,
-        data: { accessToken: undefined, userEmail: undefined },
-        message: '토큰 발급 실패'
-      };
-    }
-
-    const savedRefresh = await setRefreshToken(user.userId, refreshToken);
-
-    return savedRefresh === 'OK' // savedRefresh에서 OK 혹은 err.name반환
-      ? {
-          result: true,
-          data: { accessToken: accessToken, userEmail: user.userEmail },
-          message: '로그인 성공'
-        }
-      : {
-          result: false,
-          data: { accessToken: undefined, userEmail: undefined },
-          message: savedRefresh || 'refresh 토큰 저장 실패'
-        };
-  } catch (err) {
-    const error = ensureError(err);
-    console.log('로컬 서비스 함수 오류 : ', error.message);
+  if (typeof accessToken !== 'string' || typeof refreshToken !== 'string') {
     return {
       result: false,
       data: { accessToken: undefined, userEmail: undefined },
-      message: '로컬 로그인 중 서버 오류 ' + error
+      message: '토큰 발급 실패'
     };
   }
+
+  const savedRefresh = await setRefreshToken(user.userId, refreshToken);
+
+  return savedRefresh === 'OK' // savedRefresh에서 OK 혹은 err.name반환
+    ? {
+        result: true,
+        data: { accessToken: accessToken, userEmail: user.userEmail },
+        message: '로그인 성공'
+      }
+    : {
+        result: false,
+        data: { accessToken: undefined, userEmail: undefined },
+        message: savedRefresh || 'refresh 토큰 저장 실패'
+      };
 };
