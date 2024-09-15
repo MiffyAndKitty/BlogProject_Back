3c13bcc6515947b3472fee0d8899e0a31f5f591f
xEzIxX <by1094@gachon.ac.kr>
2024-09-15 18:34:41 +0000

    refactor: 토큰 재발급 함수에서 오류 처리 개선
    - access 토큰 decode 실패 시 InternalServerError를 throw하도록 변경
    - 유효하지 않은 refresh 토큰의 경우 BadRequestError를 throw하도록 수정
    - 전체적으로 ensureError를 통해 에러 메시지 일관성 유지

--- src/utils/token/reissueToken.ts
+++ src/utils/token/reissueToken.ts
@@ -4,6 +4,8 @@ import { newToken } from './newToken';
 import { ensureError } from '../../errors/ensureError';
 import { BasicResponse, MultipleDataResponse } from '../../interfaces/response';
 import { getRefreshToken } from '../redis/refreshToken';
+import { InternalServerError } from '../../errors/internalServerError';
+import { BadRequestError } from '../../errors/badRequestError';
 
 // 토큰 유효성 검증 후 재발급
 export async function reissueToken(
@@ -12,15 +14,13 @@ export async function reissueToken(
   try {
     const decodedId = (jwt.decode(accessToken) as JwtPayload)?.id || null;
 
-    if (!decodedId)
-      return { result: false, message: 'access 토큰 decode 실패' };
+    if (!decodedId) throw new InternalServerError('access 토큰 decode 실패');
 
     // access토큰은 유효x -> refresh토큰이 유효-> 재발급
     const refreshToken = await getRefreshToken(decodedId);
 
     // access토큰은 유효x -> refresh토큰이 유효x
-    if (!refreshToken)
-      return { result: false, message: '유효하지 않은 refresh 토큰' };
+    if (!refreshToken) throw new BadRequestError('유효하지 않은 refresh 토큰');
 
     // access토큰은 유효x-> refresh토큰이 유효-> 재발급
     const userId: string = decodedId;
@@ -35,8 +35,6 @@ export async function reissueToken(
     }
     return { result: false, message: 'access 토큰 재발급 실패' };
   } catch (err) {
-    const error = ensureError(err);
-    console.log('토큰 재발급 함수 오류 :', error.message);
-    return { result: false, message: error.message };
+    throw ensureError(err, 'access 토큰 재발급 오류');
   }
 }
