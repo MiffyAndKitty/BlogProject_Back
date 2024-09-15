4ca4b964721d880e257445d30b67f4a162f81bd1
xEzIxX <by1094@gachon.ac.kr>
2024-09-15 18:27:01 +0000

    refactor: util 파일에서 try-catch 문 추가 및 에러 처리 방식 개선
    - formatForZadd.ts, parseFieldToNumber.ts, loadYamlFiles.ts 파일에 try-catch 문 추가
    - 각 함수에서 발생할 수 있는 예외 상황에 대한 처리 강화
    - ensureError를 사용해 에러 메시지를 통일된 방식으로 처리

--- src/utils/loadYamlFiles.ts
+++ src/utils/loadYamlFiles.ts
@@ -2,15 +2,23 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 import glob from 'glob';
+import { ensureError } from '../errors/ensureError';
 
 export function loadAndCombineYamlFiles(
   directory: string
 ): Record<string, any> {
-  return glob.sync(path.join(directory, '*.yaml')).reduce((acc, file) => {
-    const content = yaml.load(fs.readFileSync(file, 'utf8')) as Record<
-      string,
-      any
-    >;
-    return { ...acc, ...content.paths };
-  }, {});
+  try {
+    return glob.sync(path.join(directory, '*.yaml')).reduce((acc, file) => {
+      const content = yaml.load(fs.readFileSync(file, 'utf8')) as Record<
+        string,
+        any
+      >;
+      return { ...acc, ...content.paths };
+    }, {});
+  } catch (err) {
+    throw ensureError(
+      err,
+      'YAML파일 파싱 중 에러 발생 : ${(err as Error).message}'
+    );
+  }
 }
--- src/utils/parseFieldToNumber.ts
+++ src/utils/parseFieldToNumber.ts
@@ -3,14 +3,22 @@ export function parseFieldToNumber<T extends Record<string, any>>(
   data: T | T[],
   fieldName: keyof T
 ): T | T[] {
-  if (Array.isArray(data)) {
-    return data.map((row) => ({
-      ...row,
-      [fieldName]: parseInt(row[fieldName] as string, 10)
-    }));
+  try {
+    if (Array.isArray(data)) {
+      return data.map((row) => ({
+        ...row,
+        [fieldName]: parseInt(row[fieldName] as string, 10)
+      }));
+    }
+    return {
+      ...data,
+      [fieldName]: parseInt(data[fieldName] as string, 10)
+    };
+  } catch (err) {
+    console.error(
+      `필드 "${String(fieldName)}"를 숫자로 변환하는 중 오류 발생:`,
+      err
+    );
+    return data; // 변환 실패 시 원래 데이터를 반환
   }
-  return {
-    ...data,
-    [fieldName]: parseInt(data[fieldName] as string, 10)
-  };
 }
