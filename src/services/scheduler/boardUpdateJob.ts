e66630388f67261b5880a6aaf389f17988e2fa83
xEzIxX <by1094@gachon.ac.kr>
2024-09-15 17:28:21 +0000

    refactor : scheduler 서비스 함수 에러 처리 로직 개선
    - public 메소드의 불필요한 try, catch 삭제
    - private 메소드의 try, catch 추가 및 ensureError 메소드를 통한 에러 처리 로직 개선

--- src/services/scheduler/boardUpdateJob.ts
+++ src/services/scheduler/boardUpdateJob.ts
@@ -6,57 +6,52 @@ export class BoardUpdateJobService {
   static async updateBoard(
     keyname: typeof CacheKeys.BOARD_VIEW | typeof CacheKeys.BOARD_LIKE
   ): Promise<boolean> {
-    try {
-      let isSuccess: boolean = true;
-      let cursor = '0';
+    let isSuccess: boolean = true;
+    let cursor = '0';
 
-      do {
-        const [nextCursor, keys] = await redis.scan(
-          cursor,
-          'MATCH',
-          `${keyname}*`,
-          'COUNT',
-          100
-        );
-        cursor = nextCursor;
-        console.log('nextCursor, keys', nextCursor, keys);
+    do {
+      const [nextCursor, keys] = await redis.scan(
+        cursor,
+        'MATCH',
+        `${keyname}*`,
+        'COUNT',
+        100
+      );
+      cursor = nextCursor;
+      console.log('nextCursor, keys', nextCursor, keys);
 
-        for (const key of keys) {
-          const boardId = key.split(':')[1];
-          const count = await redis.scard(key);
+      for (const key of keys) {
+        const boardId = key.split(':')[1];
+        const count = await redis.scard(key);
 
-          if (count <= 0) continue;
+        if (count <= 0) continue;
 
-          try {
-            const keynameField = keyname.replace(':', '');
-            const updated = await db.query(
-              `UPDATE Board SET ${keynameField} = ${keynameField} + ? WHERE board_id = ?`,
-              [count, boardId]
-            );
+        try {
+          const keynameField = keyname.replace(':', '');
+          const updated = await db.query(
+            `UPDATE Board SET ${keynameField} = ${keynameField} + ? WHERE board_id = ?`,
+            [count, boardId]
+          );
 
-            if (updated.affectedRows > 0) {
-              await redis.unlink(key);
-            } else {
-              console.log(
-                `${keyname} Board 테이블 업데이트 실패: board_id=${boardId}`
-              );
-              isSuccess = false;
-            }
-          } catch (err) {
-            console.error(
-              `${keyname} Board 테이블 업데이트 중 오류 발생: board_id=${boardId}`,
-              err
+          if (updated.affectedRows > 0) {
+            await redis.unlink(key);
+          } else {
+            console.log(
+              `${keyname} Board 테이블 업데이트 실패: board_id=${boardId}`
             );
             isSuccess = false;
           }
-          continue; // 오류 발생 시 다음 userId로 넘어감
+        } catch (err) {
+          console.error(
+            `${keyname} Board 테이블 업데이트 중 오류 발생: board_id=${boardId}`,
+            err
+          );
+          isSuccess = false;
         }
-      } while (cursor !== '0');
+        continue; // 오류 발생 시 다음 userId로 넘어감
+      }
+    } while (cursor !== '0');
 
-      return isSuccess;
-    } catch (err) {
-      console.log(`${keyname} 업데이트 중 전체적인 오류 발생:`, err);
-      return false;
-    }
+    return isSuccess;
   }
 }
