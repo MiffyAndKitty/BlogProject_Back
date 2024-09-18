d4a7a3a68f4c049795330e835858e6628612dae5
xEzIxX <by1094@gachon.ac.kr>
2024-09-18 16:13:28 +0000

    chore : 임시 저장 서비스 메서드 getDraftList의 위치 수정
    - 임시 저장 라우터 파일에서 사용되는 순서와 일치시키기 위해 수정

--- src/services/draft.ts
+++ src/services/draft.ts
@@ -15,6 +15,79 @@ import { replaceImageUrlsWithS3Links } from '../utils/string/replaceImageUrlsWit
 import { BadRequestError } from '../errors/badRequestError';
 
 export class DraftService {
+  static getDraftList = async (draftListDto: DraftListDto) => {
+    const draftCollection = mongodb.db('board_db').collection('drafts');
+
+    const { userId, cursor, isBefore } = draftListDto;
+    const pageSize = draftListDto.pageSize ?? 100;
+
+    // 기본 검색 조건: 유저 ID
+    const query: Filter<DraftFilterDto> = { userId };
+
+    let cursorBoard = null;
+
+    if (cursor) {
+      cursorBoard = await draftCollection.findOne(
+        { _id: new ObjectId(cursor), userId: userId },
+        { projection: { updatedAt: 1, _id: 1 } } // updatedAt과 _id만 가져오기
+      );
+
+      if (!cursorBoard)
+        throw new NotFoundError(
+          '해당 커서에 해당하는 게시글을 찾을 수 없습니다.'
+        );
+    }
+
+    if (cursorBoard) {
+      query.$or = [
+        {
+          updatedAt: isBefore
+            ? { $lt: cursorBoard.updatedAt }
+            : { $gt: cursorBoard.updatedAt }
+        },
+        {
+          updatedAt: cursorBoard.updatedAt, // 같은 updatedAt을 가진 경우
+          _id: isBefore ? { $lt: cursorBoard._id } : { $gt: cursorBoard._id }
+        }
+      ];
+    }
+
+    // 전체 글 개수 계산
+    const totalCount = await draftCollection.countDocuments(query);
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    let draftList;
+
+    if (cursor && isBefore == true) {
+      draftList = await draftCollection
+        .find(query)
+        .sort({ updatedAt: 1, _id: -1 })
+        .toArray();
+
+      draftList = draftList.slice(-pageSize);
+    } else {
+      draftList = await draftCollection
+        .find(query)
+        .sort({ updatedAt: 1, _id: -1 }) // updatedAt은 내림차순, updatedAt이 동일할 때는 _id를 기준으로 오름차순 정렬
+        .limit(pageSize)
+        .toArray();
+    }
+
+    if (!draftList || draftList.length === 0) {
+      throw new NotFoundError('저장된 게시글 목록이 없습니다.');
+    }
+
+    return {
+      result: true,
+      data: {
+        list: draftList,
+        totalCount: totalCount, // 전체 글 개수
+        totalPages: totalPages // 페이지 개수
+      },
+      message: '임시 저장된 게시글 목록을 반환 성공했습니다.'
+    };
+  };
+
   static saveDraft = async (draftDto: DraftDto): Promise<BasicResponse> => {
     if (
       !draftDto.title ||
@@ -142,79 +215,6 @@ export class DraftService {
       result: true,
       data: draft,
       message: '임시 저장된 게시글 반환에 성공하였습니다.'
-    };
-  };
-
-  static getDraftList = async (draftListDto: DraftListDto) => {
-    const draftCollection = mongodb.db('board_db').collection('drafts');
-
-    const { userId, cursor, isBefore } = draftListDto;
-    const pageSize = draftListDto.pageSize ?? 100;
-
-    // 기본 검색 조건: 유저 ID
-    const query: Filter<DraftFilterDto> = { userId };
-
-    let cursorBoard = null;
-
-    if (cursor) {
-      cursorBoard = await draftCollection.findOne(
-        { _id: new ObjectId(cursor), userId: userId },
-        { projection: { updatedAt: 1, _id: 1 } } // updatedAt과 _id만 가져오기
-      );
-
-      if (!cursorBoard)
-        throw new NotFoundError(
-          '해당 커서에 해당하는 게시글을 찾을 수 없습니다.'
-        );
-    }
-
-    if (cursorBoard) {
-      query.$or = [
-        {
-          updatedAt: isBefore
-            ? { $lt: cursorBoard.updatedAt }
-            : { $gt: cursorBoard.updatedAt }
-        },
-        {
-          updatedAt: cursorBoard.updatedAt, // 같은 updatedAt을 가진 경우
-          _id: isBefore ? { $lt: cursorBoard._id } : { $gt: cursorBoard._id }
-        }
-      ];
-    }
-
-    // 전체 글 개수 계산
-    const totalCount = await draftCollection.countDocuments(query);
-    const totalPages = Math.ceil(totalCount / pageSize);
-
-    let draftList;
-
-    if (cursor && isBefore == true) {
-      draftList = await draftCollection
-        .find(query)
-        .sort({ updatedAt: 1, _id: -1 })
-        .toArray();
-
-      draftList = draftList.slice(-pageSize);
-    } else {
-      draftList = await draftCollection
-        .find(query)
-        .sort({ updatedAt: 1, _id: -1 }) // updatedAt은 내림차순, updatedAt이 동일할 때는 _id를 기준으로 오름차순 정렬
-        .limit(pageSize)
-        .toArray();
-    }
-
-    if (!draftList || draftList.length === 0) {
-      throw new NotFoundError('저장된 게시글 목록이 없습니다.');
-    }
-
-    return {
-      result: true,
-      data: {
-        list: draftList,
-        totalCount: totalCount, // 전체 글 개수
-        totalPages: totalPages // 페이지 개수
-      },
-      message: '임시 저장된 게시글 목록을 반환 성공했습니다.'
     };
   };
 
