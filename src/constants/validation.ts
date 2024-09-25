743ce1c65c92d697714547247ca2d603d07f754d
xEzIxX <by1094@gachon.ac.kr>
2024-09-25 00:31:17 +0000

    refactor: 필드 최대 바이트 수정 및 상수 값 조정
    - 게시글 제목 최대 바이트 `BOARD_TITLE_MAX`를 100에서 300으로 수정
    - 태그 이름 최대 바이트 `TAG_NAME_MAX`를 50에서 45로 수정
    - 카테고리 이름 최대 바이트 `CATEGORY_NAME_MAX`를 30에서 48로 수정
    - 댓글 내용 최대 바이트 `COMMENT_CONTENT_MAX`를 500에서 600으로 수정

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -3,13 +3,13 @@ export const USER_NICKNAME_MAX = 30;
 export const USER_STATUS_MESSAGE_MAX = 60;
 
 // 게시글
-export const BOARD_TITLE_MAX = 100;
+export const BOARD_TITLE_MAX = 300;
 
 // 태그
-export const TAG_NAME_MAX = 50;
+export const TAG_NAME_MAX = 45;
 
 // 카테고리
-export const CATEGORY_NAME_MAX = 30;
+export const CATEGORY_NAME_MAX = 48;
 
 // 댓글
-export const COMMENT_CONTENT_MAX = 500;
+export const COMMENT_CONTENT_MAX = 600;
