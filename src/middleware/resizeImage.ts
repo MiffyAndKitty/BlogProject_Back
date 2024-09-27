730aeff94e6e91170f5cea330619ce2d73179da4
xEzIxX <by1094@gachon.ac.kr>
2024-09-27 03:37:29 +0000

    refactor: 리사이즈된 이미지 처리 함수를 분리 및 적용
    - 이미지 리사이즈 및 품질 조정 로직을 담당하는 `processImage` 함수를 분리

--- src/middleware/resizeImage.ts
+++ src/middleware/resizeImage.ts
@@ -28,40 +28,51 @@ export const resizeImage = () => {
 
       await Promise.all(
         originalFiles.map(async (file) => {
+          // 1. S3에서 파일 가져오기
           const fileBuffer = await getFileFromS3(file);
 
-          let quality = INITIAL_QUALITY;
+          // 2. 이미지 리사이즈 및 품질 조정
+          const resizedImage = await processImage(fileBuffer);
 
-          const resizeImageBuffer = async (
-            buffer: Buffer,
-            quality: number
-          ): Promise<Buffer> => {
-            return sharp(buffer)
-              .resize({
-                width: RESIZED_IMAGE_WIDTH,
-                withoutEnlargement: true // 원본보다 큰 이미지로 확대를 방지
-              })
-              .jpeg({ quality })
-              .toBuffer();
-          };
-
-          let resizedImage = await resizeImageBuffer(fileBuffer, quality);
-
-          while (resizedImage.length > MAX_FILE_SIZE && quality > MIN_QUALITY) {
-            quality -= QUALITY_DECREMENT;
-            resizedImage = await resizeImageBuffer(fileBuffer, quality);
-          }
-
+          // 3. 리사이즈된 이미지 업로드
           const resizedFileURL = await uploadResizedImage(file, resizedImage);
           resizedFileUrls.push(resizedFileURL);
 
+          // 4. 원본 파일 삭제
           await deleteOriginalFile(file);
         })
       );
+
       req.fileURL = resizedFileUrls;
       next();
     } catch (err) {
       next(new InternalServerError('파일 처리 중 오류 발생'));
     }
   };
+};
+
+const processImage = async (fileBuffer: Buffer): Promise<Buffer> => {
+  let quality = INITIAL_QUALITY;
+
+  const resizeImageBuffer = async (
+    buffer: Buffer,
+    quality: number
+  ): Promise<Buffer> => {
+    return sharp(buffer)
+      .resize({
+        width: RESIZED_IMAGE_WIDTH,
+        withoutEnlargement: true // 원본보다 큰 이미지로 확대를 방지
+      })
+      .jpeg({ quality })
+      .toBuffer();
+  };
+
+  let resizedImage = await resizeImageBuffer(fileBuffer, quality);
+
+  while (resizedImage.length > MAX_FILE_SIZE && quality > MIN_QUALITY) {
+    quality -= QUALITY_DECREMENT;
+    resizedImage = await resizeImageBuffer(fileBuffer, quality);
+  }
+
+  return resizedImage;
 };
